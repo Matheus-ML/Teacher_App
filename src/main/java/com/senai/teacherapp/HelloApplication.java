fd2749079e85b5f011697362e2a103a7108f40fa
BrunoRprogm <brunoramonnn986@gmail.com>
2025-05-23 21:51:23 -0400

    Feito troca Scene para Parent Root,e feito stly css do login

--- src/main/java/com/senai/teacherapp/HelloApplication.java
+++ src/main/java/com/senai/teacherapp/HelloApplication.java
@@ -2,6 +2,7 @@ package com.senai.teacherapp;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -11,8 +12,9 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Parent root = fxmlLoader.load(); //Condensa minha tela para o tamanho que coloquei no scene builder
+        Scene scene = new Scene(root);
+        stage.setTitle("TeacherApp");
         stage.setScene(scene);
         stage.show();
     }
