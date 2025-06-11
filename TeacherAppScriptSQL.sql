cf0ca6603855fc2541f86f54b58f6c2ca8bba1e8
matheus_leite <matheusleite1216@gmail.com>
2025-06-11 10:59:04 -0300

    Ajuste do sql

--- TeacherAppScriptSQL.sql
+++ TeacherAppScriptSQL.sql
@@ -1,34 +1,35 @@
 CREATE DATABASE TeacherApp;
 
+CREATE TABLE login
+(
+    cd_login INTEGER AUTO_INCREMENT,
+    id_user VARCHAR(50) UNIQUE,
+    id_password VARCHAR(16) NOT NULL,
+    id_nameProfessor VARCHAR(100) NOT NULL,
+    PRIMARY KEY(cd_login)
+);
 CREATE TABLE SchoolClass
 (
-cd_schoolclass INTEGER AUTO_INCREMENT,
-nm_schoolclass VARCHAR(40) NOT NULL,
-qt_student INTEGER NOT NULL,
-cd_login INTEGER,
-PRIMARY KEY(cd_schoolclass),
+    cd_schoolclass INTEGER AUTO_INCREMENT,
+    nm_schoolclass VARCHAR(40) NOT NULL,
+    qt_student INTEGER NOT NULL,
+    cd_login INTEGER,
+    PRIMARY KEY(cd_schoolclass),
     FOREIGN KEY (cd_login) REFERENCES login (cd_login) ON DELETE CASCADE
 );
 
 CREATE TABLE Activity
 (
-cd_activity INTEGER AUTO_INCREMENT,
-nm_activity VARCHAR(100) NOT NULL,
-ds_activity VARCHAR(255),
-dt_activity DATE NOT NULL,
-cd_schoolclass INTEGER,
-PRIMARY KEY (cd_activity),
-FOREIGN KEY(cd_schoolclass) REFERENCES schoolclass (cd_schoolclass) ON DELETE CASCADE
+    cd_activity INTEGER AUTO_INCREMENT,
+    nm_activity VARCHAR(100) NOT NULL,
+    ds_activity VARCHAR(255),
+    dt_activity DATE NOT NULL,
+    cd_schoolclass INTEGER,
+    PRIMARY KEY (cd_activity),
+    FOREIGN KEY(cd_schoolclass) REFERENCES schoolclass (cd_schoolclass) ON DELETE CASCADE
 );
 
-CREATE TABLE login 
-(
-cd_login INTEGER AUTO_INCREMENT,
-id_user VARCHAR(50) UNIQUE,
-id_password VARCHAR(16) NOT NULL,
-id_nameProfessor VARCHAR(100) NOT NULL,
-PRIMARY KEY(cd_login)
-);
+
 
 -- INSERT INTO login (id_user, id_password,id_nameProfessor) VALUES
 -- ("Karize", "karize123", "Angelica Karize Viecelli"),
